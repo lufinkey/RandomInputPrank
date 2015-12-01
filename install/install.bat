7f5a49c087d2f4332742fe2a67f832f210334a6c
Luis Finke <luisfinke@gmail.com>
2015-12-01 10:27:59 -0500

    Fixed install script

--- install/install.bat
+++ install/install.bat
@@ -3,5 +3,6 @@ mkdir "%userprofile%\AppData\Local\lufinkey"
 mkdir "%userprofile%\AppData\Local\lufinkey\rip"
 copy /y "%~dp0\defaultsettings.ini" "%userprofile%\AppData\Local\lufinkey\rip\settings.ini"
 copy /y "%~dp0\rip.exe" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe"
-START "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe"
+attrib "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe" +h
+start "" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe"
 exit
