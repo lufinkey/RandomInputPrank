c651efbd776cac6061cc225cb69f77b46abf9153
Luis Finke <luisfinke@gmail.com>
2015-12-01 13:22:18 -0500

    created uninstall script

--- install/install.bat
+++ install/install.bat
@@ -2,7 +2,7 @@ taskkill /f /IM rip.exe
 mkdir "%userprofile%\AppData\Local\lufinkey"
 mkdir "%userprofile%\AppData\Local\lufinkey\rip"
 copy /y "%~dp0\defaultsettings.ini" "%userprofile%\AppData\Local\lufinkey\rip\settings.ini"
+del /a "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe"
 copy /y "%~dp0\rip.exe" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe"
 attrib "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe" +h
 start "" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe"
-exit
---
+++ install/uninstall.bat
@@ -0,0 +1,2 @@
+taskkill /f /IM rip.exe
+del /a "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rip.exe"
